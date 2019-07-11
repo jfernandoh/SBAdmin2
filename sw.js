a875f29c282ca89e572d3750c908236aeb328d72
Fernando H <consultordev@quimicasuiza.com>
2019-07-11 11:38:30 -0500

    v8

--- sw.js
+++ sw.js
@@ -6,10 +6,24 @@ const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
-
 const APP_SHELL = [
     // '/',
-    'index.html'
+    'index.html',
+    '404.html',
+    'blank.html',
+    'buttons.html',
+    'cards.html',
+    'charts.html',
+    'forgot-password.html',
+    'login.html',
+    'register.html',
+    'tables.html',
+    'utilities-animation.html',
+    'utilities-border.html',
+    'utilities-color.html',
+    'utilities-other.html',
+    'js/sw-utils.js',
+    'js/app.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -27,17 +41,17 @@ const APP_SHELL_INMUTABLE = [
 
 self.addEventListener('install', e => {
 
-    // const cacheStatic = caches.open( STATIC_CACHE )
-    //     .then( cache => {
-    //         return cache.addAll( APP_SHELL );
-    //     });
+    const cacheStatic = caches.open( STATIC_CACHE )
+        .then( cache => {
+            return cache.addAll( APP_SHELL );
+        });
     
-    // const cacheInmutable = caches.open( INMUTABLE_CACHE )
-    //     .then( cache => {
-    //         return cache.addAll( APP_SHELL_INMUTABLE );
-    //     });
+    const cacheInmutable = caches.open( INMUTABLE_CACHE )
+        .then( cache => {
+            return cache.addAll( APP_SHELL_INMUTABLE );
+        });
 
-    // e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ]) );
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ]) );
 });
 
 self.addEventListener('activate', e => {
